b4e839296609bc7aba47e41bba20d9c27a51b726
ryanjameskennedy <ryanjameskennedy@icloud.com>
2023-08-25 13:49:55 +0200

    Add check exists method

--- mongtool/validate.py
+++ mongtool/validate.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from mongtool.database import Database
 
@@ -7,6 +8,9 @@ class Validate(object):
 
     def get_species_name(self, results):
         return results["species_prediction"][0]["scientific_name"]
+
+    def _check_exists(self, db_collection, sample_id):
+        return (True if list(Database.find(db_collection, {"id": sample_id})) else False)
 
     def search(self, search_query, search_kw, search_list):
         return [element for element in search_list if element[search_kw] == search_query]
@@ -29,20 +33,20 @@ class Validate(object):
         mdb_mlst = list(Database.get_mlst(db_collection, {"id": sample_id}))
         mdb_cgmlst = list(Database.get_cgmlst(db_collection, {"id": sample_id}))
         mdb_pvl_present = int(mdb_pvl[0]["aribavir"]["lukS_PV"]["present"])
-        mdb_mlst_seqtype = int(mdb_mlst[0]["mlst"]["sequence_type"])
+        mdb_mlst_seqtype = str(mdb_mlst[0]["mlst"]["sequence_type"])
         mdb_mlst_alleles = mdb_mlst[0]["mlst"]["alleles"]
         mdb_cgmlst_alleles = mdb_cgmlst[0]["alleles"]
         return {"pvl": mdb_pvl_present, "mlst_seqtype": mdb_mlst_seqtype, "mlst_alleles": mdb_mlst_alleles, "cgmlst_alleles": mdb_cgmlst_alleles}
-    
+
     def get_fin_data(self, sample_json):
         fin_pvl_present = self.get_pvl(sample_json)
         fin_mlst = self.get_mlst(sample_json)
         fin_cgmlst = self.get_cgmlst(sample_json)
-        fin_mlst_seqtype = fin_mlst[0]["result"]["sequence_type"]
+        fin_mlst_seqtype = str(fin_mlst[0]["result"]["sequence_type"])
         fin_mlst_alleles = fin_mlst[0]["result"]["alleles"]
         fin_cgmlst_alleles = list(fin_cgmlst[0]["result"]["alleles"].values())
         return {"pvl": fin_pvl_present, "mlst_seqtype": fin_mlst_seqtype, "mlst_alleles": fin_mlst_alleles, "cgmlst_alleles": fin_cgmlst_alleles}
-    
+
     def compare_mlst_alleles(self, old_mlst_alleles, new_mlst_alleles):
         match_count, total_count = 0, 0
         for allele in old_mlst_alleles:
@@ -71,6 +75,9 @@ class Validate(object):
             with open(input_file, 'r') as fin:
                 sample_json = json.load(fin)
                 sample_id = self.get_sample_id(sample_json)
+                if not self._check_exists(db_collection, sample_id):
+                    print(f"The sample provided ({sample_id}) does not exist in the provided database ({Database.db_name}) or collection ({db_collection}).")
+                    continue
                 mdb_data_dict = self.get_mdb_cgv_data(db_collection, sample_id)
                 species_name = self.get_species_name(sample_json)
                 fin_data_dict = self.get_fin_data(sample_json)
