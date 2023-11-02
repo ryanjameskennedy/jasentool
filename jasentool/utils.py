15ddd3d36f09407b31c2e7a6f723e0882b62a654
ryanjameskennedy <ryanjameskennedy@icloud.com>
2023-11-02 13:46:35 +0100

    Add pipeline ready check to auto-start

--- jasentool/utils.py
+++ jasentool/utils.py
@@ -25,29 +25,39 @@ class Utils(object):
             fout.write(output_txt)
 
     @staticmethod
-    def copy_batch_and_csv_files(batch_files, csv_files, output_dir, remote_hostname, remote=False):
+    def pipeline_ready(batch_file):
+        assays = ['saureus']
+        for assay in assays:
+            if assay in batch_file:
+                return True
+        return False
+
+    @staticmethod
+    def copy_batch_and_csv_files(batch_files, csv_files, remote_dir, remote_hostname, remote=False):
         if remote:
             # Copy files to remote server using ssh/scp
             process = subprocess.run(
-                f'ssh {remote_hostname} mkdir -p {output_dir}',
+                f'ssh {remote_hostname} mkdir -p {remote_dir}',
                 shell=True
             )
             process = subprocess.run(
-                f'scp {" ".join(batch_files)} {" ".join(csv_files)} {remote_hostname}:{output_dir}',
+                f'scp {" ".join(batch_files)} {" ".join(csv_files)} {remote_hostname}:{remote_dir}',
                 shell=True,
                 stdout=subprocess.PIPE,
                 universal_newlines=True
             )
         else:
             # Copy files to a local directory
-            pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+            pathlib.Path(remote_dir).mkdir(parents=True, exist_ok=True)
             for fn in batch_files + csv_files:
-                shutil.copy(fn, output_dir)
+                shutil.copy(fn, remote_dir)
 
+    @staticmethod
     def start_remote_pipelines(batch_files, remote_hostname, remote_dir):
         for batch_file in batch_files:
-            sleep(10.0) # Avoid maxing SSH auth connections
-            process = subprocess.Popen(
-                ["ssh", remote_hostname, "bash", f"{remote_dir}/{os.path.basename(batch_file)}"],
-                close_fds=True
-            )
+            if Utils.pipeline_ready(batch_file):
+                sleep(10.0) # Avoid maxing SSH auth connections
+                process = subprocess.Popen(
+                    ["ssh", remote_hostname, "bash", f"{remote_dir}/{os.path.basename(batch_file)}"],
+                    close_fds=True
+                )
