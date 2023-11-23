244276717defdf8300c50c2db512b1e5f530163b
ryanjameskennedy <ryanjameskennedy@icloud.com>
2023-11-23 17:35:48 +0100

    Add download_gff to genome.py

--- jasentool/genome.py
+++ jasentool/genome.py
@@ -1,13 +1,17 @@
 import os
 from Bio import Entrez, SeqIO
+from jasentool.utils import Utils
 
 class Genome:
     def __init__(self, refseq_accn, genbank_accn, download_dir, prefix, email="rjkennedyy@gmail.com"):
         Entrez.email = email
         self.refseq_accn = refseq_accn
         self.genbank_accn = genbank_accn
+        self.download_dir = download_dir
+        self.zip_filepath = os.path.join(download_dir, "GCF_000195955.2.zip")
         self.fasta_filepath = os.path.join(download_dir, f"{prefix}.fasta")
         self.genbank_filepath = os.path.join(download_dir, f"{prefix}.gb")
+        self.gff_filepath = os.path.join(download_dir, f"{prefix}.gff")
 
     def download_fasta(self):
         try:
@@ -40,3 +44,16 @@ class Genome:
         except Exception as e:
             print(f"Error downloading the genbank file: {e}")
         return self.genbank_filepath
+    
+    def download_gff(self):
+        utils = Utils()
+        h37rv_url = "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000195955.2/download?include_annotation_type=GENOME_GFF&filename=GCF_000195955.2.zip"
+        try:
+            utils.download_and_save_file(h37rv_url, self.zip_filepath)
+            utils.unzip(self.zip_filepath, self.download_dir)
+            source = os.path.join(self.download_dir, "ncbi_dataset/data/GCF_000195955.2/genomic.gff")
+            destination = os.path.join(self.download_dir, "h37rv.gff")
+            utils.copy_file(source, destination)
+        except Exception as e:
+            print(f"Error downloading the gff file: {e}")
+        return self.gff_filepath
