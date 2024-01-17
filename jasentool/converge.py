fc412eb6fbf236cb68fb63b52005d34882854b7b
ryanjameskennedy <ryanjameskennedy@icloud.com>
2024-01-17 14:27:32 +0100

    Fix converge.py pylinting errors

--- jasentool/converge.py
+++ jasentool/converge.py
@@ -1,3 +1,5 @@
+"""Module to converge mutation catalogues"""
+
 import os
 import pandas as pd
 from jasentool.who import WHO
@@ -5,7 +7,8 @@ from jasentool.genome import Genome
 from jasentool.tbprofiler import Tbprofiler
 from jasentool.utils import Utils
 
-class Converge(object):
+class Converge:
+    """Class that converges mutation catalogues"""
     def __init__(self, download_dir):
         self.download_dir = download_dir
         self.fohm_fpath = os.path.join(os.path.dirname(__file__), "data/dbs/fohm.csv")
@@ -45,6 +48,7 @@ class Converge(object):
         return intersection_df, unique_tbdb_df, unique_who_df
 
     def run(self):
+        """Run the retrieval and convergance of mutation catalogues"""
         utils = Utils()
         # Download the genome
         mycobacterium_genome = Genome("NC_000962.3", "AL123456.3", self.download_dir, "h37rv")
@@ -55,14 +59,18 @@ class Converge(object):
         tbprofiler = Tbprofiler(self.tbdb_filepath)
         #h37rv_gb_filepath = mycobacterium_genome.download_genbank()
         who_df = who._parse(fasta_filepath, gff_filepath, self.download_dir)
-        tbdb_df = tbprofiler._parse(fasta_filepath, gff_filepath, self.download_dir)
-        #tbdb_df, who_df = pd.read_csv("/data/bnf/dev/ryan/pipelines/jasen/converge/tbdb.csv"), pd.read_csv("/data/bnf/dev/ryan/pipelines/jasen/converge/who.csv")
+        tbdb_df = tbprofiler._parse(self.download_dir)
+        #tbdb_df = pd.read_csv("/data/bnf/dev/ryan/pipelines/jasen/converge/tbdb.csv")
+        #who_df = pd.read_csv("/data/bnf/dev/ryan/pipelines/jasen/converge/who.csv")
         fohm_df = pd.read_csv(self.fohm_fpath)
-        intersection_df, unique_tbdb_df, unique_who_df = self.compare_columns(tbdb_df, who_df, ['Drug', 'Gene', 'Mutation'])
-        fohm_tbdb_df = pd.concat([intersection_df, unique_tbdb_df, fohm_df], ignore_index=True).drop_duplicates()
+        column_names = ['Drug', 'Gene', 'Mutation']
+        intersection_df, unique_tbdb_df, unique_who_df = self.compare_columns(tbdb_df, who_df, column_names)
+        dfs_to_concat = [intersection_df, unique_tbdb_df, fohm_df]
+        fohm_tbdb_df = pd.concat(dfs_to_concat, ignore_index=True).drop_duplicates()
         intersection_df.to_csv(self.intersection_outfpath, index=False)
         unique_tbdb_df.to_csv(self.unique_tbdb_outfpath, index=False)
         unique_who_df.to_csv(self.unique_who_outfpath, index=False)
         fohm_tbdb_df.to_csv(self.fohm_tbdb_outfpath, index=False)
-        converged_df = pd.concat([intersection_df, unique_tbdb_df, unique_who_df, fohm_df], ignore_index=True).drop_duplicates()
+        dfs_to_converge = [intersection_df, unique_tbdb_df, unique_who_df, fohm_df]
+        converged_df = pd.concat(dfs_to_converge, ignore_index=True).drop_duplicates()
         converged_df.to_csv(self.convereged_outfpath, index=False)
